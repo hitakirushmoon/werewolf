7e04050cf9e17d7961ea6b3bb848bc92e67e3c95
hitakirushmoon <minhmarty@gmail.com>
2022-02-13 22:04:54 +0700

    io?

--- server/server.js
+++ server/server.js
@@ -1,11 +1,14 @@
 const http = require('http')
 const express = require('express')
+const socketio = require('socket.io')
+
 
 const app = express()
 
 app.use(express.static(`${__dirname}/../client`))
 
 const server = http.createServer(app)
+const io = socketio(server);
 
 server.on('error', (err) => {
     console.error(err)
@@ -13,4 +16,4 @@ server.on('error', (err) => {
 server.listen(process.env.PORT, () => {
     console.log("yay server is ready \\o/")
 })
-// const io = require('socket.io')
+
